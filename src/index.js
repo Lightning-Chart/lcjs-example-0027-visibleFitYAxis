adcdc5752039e51a2e46a243d14ee8245a664950
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-05 15:10:19 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const xydata = require('@arction/xydata')
 const { lightningChart, AxisScrollStrategies, Themes } = lcjs
 const { createProgressiveTraceGenerator } = xydata
 
-const chart = lightningChart()
+const chart = lightningChart({
+            resourcesBaseUrl: new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'resources/',
+        })
     .ChartXY({
         theme: Themes[new URLSearchParams(window.location.search).get('theme') || 'darkGold'] || undefined,
     })
